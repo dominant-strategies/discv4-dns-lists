d60b2de30cfd28124e58950eae6dccd3dcecd5e3
Alan Orwick <alan.kev11@gmail.com>
2022-12-13 08:55:31 -0600

    fix array

--- .ci/crawl.sh
+++ .ci/crawl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-declare -a arr=("30303 prime" "30304 cyprus" "30305 paxos" "30306 hyrda" "30307 cyprus1" "30308 cyprus2" "30309 cyprus3" "30310 paxos1" "30311 paxos2" "30312 paxos3" "30313 hydra1" "30314 hyrda2" "30315 hyrda3")
+arr=("30303 prime" "30304 cyprus" "30305 paxos" "30306 hyrda" "30307 cyprus1" "30308 cyprus2" "30309 cyprus3" "30310 paxos1" "30311 paxos2" "30312 paxos3" "30313 hydra1" "30314 hyrda2" "30315 hyrda3")
 
 mkdir -p all-nodes
 for net in "${arr[@]}"
