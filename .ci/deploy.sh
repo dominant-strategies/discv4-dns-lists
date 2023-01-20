ac96fc2dce71aad00046be754015fdec36bb9dce
Alan Orwick <alan.kev11@gmail.com>
2023-01-20 15:54:37 -0600

    Add folder prefix, fix deploy, and rename nets

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -6,16 +6,14 @@ chains=("30303 prime" "30304 cyprus" "30305 paxos" "30306 hyrda" "30307 cyprus1"
 networks=(colosseum garden)
 
 for chain in "${chains[@]}"; do
-
-    echo "Deploy: $network"
-
+    set -- $chain
     for network in "${networks[@]}"; do
-        echo -n "Deploy: ${proto}.${network}.${QUAI_DNS_DISCV4_PARENT_DOMAIN}"
+        
+        echo -n "Deploy: $2.${network}.${QUAI_DNS_DISCV4_PARENT_DOMAIN}"
 
         # Ensure that we actually have a nodeset to deploy to DNS.
-        [[ ! -d ${chain}.${network}.${QUAI_DNS_DISCV4_PARENT_DOMAIN} ]] || [[ ! -f ${chain}.${network}.${QUAI_DNS_DISCV4_PARENT_DOMAIN}/nodes.json ]] && { echo " | DNE, skipping"; continue; }
-
+        [[ ! -d ${network}/$2.${network}.${QUAI_DNS_DISCV4_PARENT_DOMAIN} ]] || [[ ! -f ${network}/$2.${network}.${QUAI_DNS_DISCV4_PARENT_DOMAIN}/nodes.json ]] && { echo " | DNE, skipping"; continue; }
         echo
-        ./devp2p dns to-cloudflare --zoneid "$QUAI_DNS_CLOUDFLARE_ZONEID" "${chain}.${network}.${QUAI_DNS_DISCV4_PARENT_DOMAIN}"
+        ./devp2p dns to-cloudflare --zoneid "$QUAI_DNS_CLOUDFLARE_ZONEID" "${network}/$2.${network}.${QUAI_DNS_DISCV4_PARENT_DOMAIN}"
     done
 done
